5b47db1018b95db750467ec26c83de9601a2f4ca
Tong Zifei <chevalun@gmail.com>
2010-02-22 20:40:02 +0800

    inflate, AC.

--- usaco/inflate.c
+++ usaco/inflate.c
@@ -5,20 +5,21 @@ TASK: inflate
 */
 #include <stdio.h>
 
-long long dp[10001];
+int dp[10001], v[10001], m[10001];
 
 int main()
 {
 	FILE *fi = fopen("inflate.in", "r");
 	FILE *fo = fopen("inflate.out", "w");
-	int N, i, j, V, o[25];
+	int N, i, j, V;
 	fscanf(fi, "%d %d", &V, &N);
-	for(i = 0; i < V; ++i)
-		fscanf(fi, "%d", &o[i]);
-	dp[0] = 1;	
-	for(i = 0; i < V; ++i)
-		for(j = o[i]; j <= N; ++j)
-			dp[j] += dp[j-o[i]];
-	fprintf(fo, "%lld\n", dp[N]);
+	for(i = 0; i < N; ++i)
+		fscanf(fi, "%d %d", &v[i], &m[i]);
+	dp[0] = 0;	
+	for(j = 0; j < N; ++j)
+		for(i = m[j]; i <= V; ++i)
+			if (dp[i] < dp[i-m[j]] + v[j])
+				dp[i] = dp[i-m[j]] + v[j];
+	fprintf(fo, "%d\n", dp[V]);
 	return 0;	
 }
