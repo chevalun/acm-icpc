36383d12a78d4a0c90d7a56581bac0bf8e0fe1fb
Tong Zifei <chevalun@gmail.com>
2010-02-23 15:04:30 +0800

    stamps, AC.

--- usaco/fact4.c
+++ usaco/fact4.c
@@ -5,11 +5,27 @@ TASK: fact4
 */
 #include <stdio.h>
 
-long long dp[10001];
-
 int main()
 {
 	FILE *fi = fopen("fact4.in", "r");
 	FILE *fo = fopen("fact4.out", "w");
+	int i, N, rst = 1, j, c = 0;
+	int num[5000];
+	fscanf(stdin, "%d", &N);
+	for(i = 1; i <= N; ++i)
+		num[i] = i;
+	for(i = 5; i <= N; i+=5)
+		while (num[i] % 5 == 0) {
+			num[i] /= 5;
+			++c;
+		}
+	for(i = 2; i <= N; i+=2)
+		while ((num[i] % 2 == 0) && c) {
+			num[i] /= 2;
+			--c;
+		}
+	for(i = 1; i <= N; ++i)
+		rst = num[i] * rst % 10;		
+	fprintf(stdout, "%d\n", rst);
 	return 0;	
 }
--- usaco/stamps.c
+++ usaco/stamps.c
@@ -5,28 +5,29 @@ TASK: stamps
 */
 #include <stdio.h>
 
-int dp[100001];
+int dp[2000002];
 
 int main()
 {
 	FILE *fi = fopen("stamps.in", "r");
 	FILE *fo = fopen("stamps.out", "w");
-	int N, K, stamp[50], i, j, m;
+	int N, K, stamp[50], i, j, m, max = 0;
 	fscanf(fi, "%d %d", &K, &N);
-	for(i = 0; i < N; ++i)
+	for(i = 0; i < N; ++i) {
 		fscanf(fi, "%d", &stamp[i]);
-	m = 0;
-	for(i = 1; i < 100000; ++i)
-		dp[i] = 500;
-	for(;;) {
-		++m;
-		for(i = 0; i < N; ++i)
-			for(j = stamp[i]; j <= m; ++j)
-				if (dp[j] > dp[j-stamp[i]] + 1)
-					dp[j] = dp[j-stamp[i]] + 1;
-		if (dp[m] > K)
-			break;
+		if (stamp[i] > max)
+			max = stamp[i];
 	}
-	fprintf(fo, "%d\n", m-1);
+	m = max * K + 1;
+	for(i = 1; i < m; ++i)
+		dp[i] = 500;
+	for(i = 0; i < N; ++i)
+		for(j = stamp[i]; j <= m; ++j)
+			if (dp[j] > dp[j-stamp[i]] + 1)
+				dp[j] = dp[j-stamp[i]] + 1;
+	for(i = 1; i < m; ++i)
+		if (dp[i] > K)
+			break;
+	fprintf(fo, "%d\n", i-1);
 	return 0;	
 }
