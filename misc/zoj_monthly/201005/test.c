c4aced1f83c99e3271cda837706da4d94c9ba01a
Zifei Tong <chevalun@gmail.com>
2010-09-06 03:10:48 +0800

    cleanup

--- misc/zoj_monthly/201005/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int i;
-	printf("100000\n");
-	for(i = 1; i <= 100000; ++i)
-		printf("%d\n", i);
-}
--- usaco/fence8.cpp
+++ usaco/fence8.cpp
@@ -1,36 +1,78 @@
 /*
 ID: chevalu2
-LANG: C++
 TASK: fence8
+LANG: C++
 */
 #include <iostream>
 #include <fstream>
 #include <algorithm>
 using namespace std;
 
+int N, space, mid, sum, rsum[2000], bag[100], r[2000];
+
+bool dfsid(int now, int last)
+{
+	if (now <= 0)
+		return true;
+	if (space > sum - rsum[mid])
+		return false;
+	for(int i = last; i <= N; ++i)
+		if(bag[i] >= r[now]) {
+			bag[i] -= r[now];
+			if(bag[i] < r[1])
+				space += bag[i];
+			if(r[now] == r[now-1]) {
+				if(dfsid(now-1, i))
+					return true;
+			} else {
+				if (dfsid(now-1, 1))
+					return true;
+			}
+			if(bag[i] < r[1])
+				space -= bag[i];
+			bag[i] += r[now];
+		}
+	return false;	
+}
+
 int main()
 {
-	ifstream stdin("fence8.in");
-	ofstream stdout("fence8.out");
+	ifstream cin("fence8.in");
+	ofstream cout("fence8.out");
 	
-	int N, sum, b[51];
-	stdin >> N;
-	for(int i = 0; i < N; ++i) {
-		stdin >> b[i];
+	int b[51];
+	cin >> N;
+	for(int i = 1; i <= N; ++i) {
+		cin >> b[i];
 		sum += b[i];
 	}
 	
-	int R, r[1024], rsum[1024];
-	stdin >> R;
-	for(int i = 0; i < R; ++i)
-		stdin >> r[i];
-	rsum[0] = r[0];	
-	for(int i = 1; i < R; ++i)
+	int R;
+	cin >> R;
+	for(int i = 1; i <= R; ++i)
+		cin >> r[i];
+	sort(b, b + N + 1);
+	sort(r, r + R + 1);
+	r[0] = b[0] = 0;
+	for(int i = 1; i <= R; ++i)
 		rsum[i] = rsum[i-1] + r[i];
-	sort(b, b + N);
-	sort(r, r + R);	
-	
 	while(rsum[R] > sum)
 		--R;
+	
+	int left = 0, right = R;
+	mid = (left + right) / 2;
+	while(left <= right) {
+		for(int i = 1; i <= N; ++i)
+			bag[i] = b[i];
+		space = 0;	
+		if(dfsid(mid, 1)) {
+			left = mid + 1;
+			mid = (left + right) / 2;
+		} else {
+			right = mid - 1;
+			mid = (left + right)/2;
+		}
+	}
+	cout << mid << endl;
 	return 0;	
 }
